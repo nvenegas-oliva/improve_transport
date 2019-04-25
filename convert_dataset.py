94ccdfacb3e39e8628c6c4860f40a3924cfe7fab
Nicolas Venegas Oliva <nvenegas.oliva@gmail.com>
2019-04-25 15:49:59 -0400

    feat: Remove spark use

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,13 +1,9 @@
 
-import os
 from io import BytesIO
 from zipfile import ZipFile, BadZipfile
 from datetime import datetime
 import re
 import argparse
-
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext
 
 
 def build_filename(raw_name):
@@ -42,11 +38,6 @@ def prepare_csv(file_name, table):
 
 
 def main(args):
-    conf = SparkConf()
-    sc = SparkContext(conf=conf)
-    print(sc._conf.getAll())
-
-    sqlContext = SQLContext(sc)
 
     if args.environment == "local":
         input_path = "datasets/test-folder/*.zip"
@@ -55,63 +46,11 @@ def main(args):
         input_path = "s3n://dtpm-transactions/test-folder/*.zip"
         output_path = "s3n://dtpm-transactions/parquet/"
 
-    rdd = sc.binaryFiles(input_path).flatMap(lambda a: extract_files(a[0], a[1]))
-
-    # Decode bytes and convert it in a list of strings
-    rdd = rdd.mapValues(lambda file: BytesIO(file).read().decode('cp1252').split('\n'))
-
-    # Drop header and last (and empty) row
-    rdd = rdd.mapValues(lambda table: table[1:-1])
-
-    # Change type of columns
-    rdd = rdd.flatMap(lambda a: prepare_csv(a[0], a[1]))
-
-    header = ['FILE_NAME',
-              'FECHAHORATRX',
-              'CODIGOENTIDAD',
-              'NOMBREENTIDAD',
-              'CODIGOSITIO',
-              'NOMBRESITIO',
-              'NROTARJETA']
-    header = list(map(lambda a: a.lower(), header))
-
-    df = rdd.toDF(header)
-    print("df.rdd.getNumPartitions()=%d" % df.rdd.getNumPartitions())
-    df = df.repartition(100)
-    print("df.rdd.getNumPartitions()=%d" % df.rdd.getNumPartitions())
-    print("df.count()=%d" % df.count())
-    days = [file.file_name for file in df.select('file_name').distinct().collect()]
-    print("days=%s" % days)
-    """for directory in days:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        df_day = df.select(df.columns[1:]).where(df.file_name == directory)
-        df_day.write.parquet(output_path + directory + "/data.parquet", compression="gzip")"""
-
 
 if __name__ == "__main__":
-
-    """conf = SparkConf().setMaster("local[4]").setAppName("transport")
-    sc = SparkContext(conf=conf)
-
-    # sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", "AKIAI6YJJLTG7TUBMVXA")
-    # sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", "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")
-
-    sqlContext = SQLContext(sc)
-
-    input_path = "s3n://dtpm-transactions/test-folder-small/*.zip"
-    # output_path = "s3://dtpm-transactions/parquet/"
-    rdd = sc.binaryFiles(input_path)
-    rdd.collect()
-    print("rdd.count()=%d" % rdd.count())"""
 
     parser = argparse.ArgumentParser()
     parser.add_argument("environment", help="'cloud' or 'local'")
     args = parser.parse_args()
     print("args.environment=" + args.environment)
     main(args)
-
-    """input_path = "s3n://dtpm-transactions/test-folder-small/*.zip"
-    rdd = sc.binaryFiles(input_path).flatMap(lambda a: extract_files(a[0], a[1]))
-    rdd.collect()
-    print("rdd.count()=%d" % rdd.count())"""
