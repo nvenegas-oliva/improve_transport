fc575ed36498511f8c3bf0045c1cfc9e2d81e5e5
Nicolas Venegas Oliva <nvenegas.oliva@gmail.com>
2019-04-25 17:04:11 -0400

    feat: Remove functions for spark

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,40 +1,15 @@
 
 from io import BytesIO
+import argparse
 from zipfile import ZipFile, BadZipfile
 from datetime import datetime
 import re
-import argparse
+import pandas as pd
 
 
 def build_filename(raw_name):
     day, from_day, to_day = re.findall(r"[\d+]{8}", raw_name)
     return "day=%s/from=%s/to=%s" % (day, from_day, to_day)
-
-
-def extract_files(compressed_name, stream):
-    """
-    Extract .csv files from a .zip file and load in different DataFrames.
-    """
-    with BytesIO(stream) as tf:
-        tf.seek(0)
-        # Read the file as a zipfile and process the members
-        try:
-            with ZipFile(tf, mode='r') as zipf:
-                return [(build_filename(compressed_name + file_name), zipf.read(file_name)) for file_name in zipf.namelist()]
-        except BadZipfile:
-            return []
-# datetime.strptime(row[0], "%d/%m/%Y %H:%M:%S")
-
-
-def prepare_csv(file_name, table):
-    try:
-        return list(map(lambda row: [file_name, datetime.strptime(row[0], "%d/%m/%Y %H:%M:%S"), int(row[1]),
-                                     row[2], int(row[3]), row[4],
-                                     row[5]], table))
-    except ValueError:
-        return list(map(lambda row: [file_name, row[0], int(row[1]),
-                                     row[2], int(row[3]), row[4],
-                                     row[5]], table))
 
 
 def main(args):
@@ -47,6 +22,29 @@ def main(args):
         output_path = "s3n://dtpm-transactions/parquet/"
 
 
+def create_df(decompressed_file):
+    """Generate a pandas DataFrame from a .csv compressed file"""
+    dtype = {
+        'CODIGOENTIDAD': 'int64',
+        'NOMBREENTIDAD': 'str',
+        'CODIGOSITIO': 'int64',
+        'NOMBRESITIO': 'str',
+        'NROTARJETA': 'str'
+    }
+    try:
+        df = pd.read_csv(
+            BytesIO(decompressed_file),
+            encoding="cp1252",
+            sep=";",
+            usecols=range(6),
+            dtype=dtype)
+    except:
+        return None
+    df["FECHAHORATRX"] = pd.to_datetime(
+        df["FECHAHORATRX"], format="%d/%m/%Y %H:%M:%S", errors='coerce')
+    return df
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -54,3 +52,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print("args.environment=" + args.environment)
     main(args)
+    path = "datasets/20181103.zip"
+    try:
+        with ZipFile(path, mode='r') as zipf:
+            decompressed_file = [(file_name, create_df(zipf.read(file_name)))
+                                 for file_name in zipf.namelist()]
+    except BadZipfile:
+        print("BadZipfile")
+    list(map(lambda a: a[0], decompressed_file))
+    decompressed_file[0][1].info()
