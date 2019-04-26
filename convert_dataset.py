fc58920c1bd6e932da68c0ae3d12f6195d3827af
Nicolas Venegas Oliva <nvenegas.oliva@gmail.com>
2019-04-26 18:07:32 -0400

    feat: Add logging

--- convert_dataset.py
+++ convert_dataset.py
@@ -4,7 +4,19 @@ import argparse
 from zipfile import ZipFile, BadZipfile
 import re
 from boto3 import resource
+from memory_profiler import profile
+import logging
 import pandas as pd
+
+# Set different levels of logging
+logging.basicConfig(
+    level=logging.ERROR,
+    # filename='app.log',
+    filemode='w',
+    format='[%(name)s] [%(levelname)s] [%(asctime)s] - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def build_filename(raw_name):
@@ -15,26 +27,6 @@ def build_filename(raw_name):
     return "day=%s/from=%s/to=%s" % (day, from_day, to_day)
 
 
-def main(args):
-    BUCKET = "dtpm-transactions"
-    if args.environment == "local":
-        input_path = "datasets/test-folder/*.zip"
-        output_path = "./"
-    elif args.environment == "cloud":
-        input_path = "s3n://dtpm-transactions/test-folder/*.zip"
-        output_path = "s3n://dtpm-transactions/parquet/"
-
-    # Get list of files to transform
-    file = "test-folder/20190410.zip"
-    s3 = resource("s3")
-    obj = s3.Bucket(BUCKET).Object(file)
-    with BytesIO(obj.get()["Body"].read()) as stream:
-        # Rewind the file
-        stream.seek(0)
-        convert_dataset(stream, obj.key, BUCKET)
-
-
-@profile
 def convert_dataset(dataset, file_name, bucket):
     """
     Convert dataset (from stream) to DataFrame and save as .parquet in s3.
@@ -49,7 +41,7 @@ def convert_dataset(dataset, file_name, bucket):
                 (build_filename(file_name + sub_file), create_df(zipf.read(sub_file))
                  ) for sub_file in zipf.namelist()]
     except BadZipfile:
-        print("BadZipfile")
+        logging.error("Exception occurred", exc_info=True)
 
     # Write in parquet all sub-files.
     for file_name, df in decompressed_file:
@@ -57,7 +49,6 @@ def convert_dataset(dataset, file_name, bucket):
         df.to_parquet(output_dir, compression="gzip", engine="pyarrow")
 
 
-@profile
 def create_df(decompressed_file):
     """
     Generate a pandas DataFrame using the correct format.
@@ -79,6 +70,28 @@ def create_df(decompressed_file):
         df["FECHAHORATRX"], format="%d/%m/%Y %H:%M:%S", errors='coerce')
     df.columns = [x.lower() for x in df.columns]
     return df
+
+
+def main(args):
+    logger.debug('This will get logged')
+    logger.warning('This will get logged to a file')
+    BUCKET = "dtpm-transactions"
+    if args.environment == "local":
+        input_path = "datasets/test-folder/*.zip"
+        output_path = "./"
+    elif args.environment == "cloud":
+        input_path = "s3n://dtpm-transactions/test-folder/*.zip"
+        output_path = "s3n://dtpm-transactions/parquet/"
+
+    # Get list of files to transform
+    file = "test-folder-small/20180818.zip"  # Small file
+    # file = "test-folder-small/20181011.zip"  # Zip error file
+    s3 = resource("s3")
+    obj = s3.Bucket(BUCKET).Object(file)
+    with BytesIO(obj.get()["Body"].read()) as stream:
+        # Rewind the file
+        stream.seek(0)
+        convert_dataset(stream, obj.key, BUCKET)
 
 
 if __name__ == "__main__":
